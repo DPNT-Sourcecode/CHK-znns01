9ea29e4aaa8179c1a3e4fa7c71212af6341f33cf
sbx_user1051 <sbx_user1051@169.254.235.49>
2022-05-10 23:13:52 +0000

    Tue May 10 23:13:52 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,5 +2,17 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
-def checkout(skus):
+
+def prices_offers_list() -> list(dict):
+    prices_offers = [
+        {"item": "A", "price": 50, "offer": "3A for 130"},
+        {"item": "B", "price": 30, "offer": "2B for 45"},
+        {"item": "C", "price": 20},
+        {"item": "D", "price": 15},
+    ]
+
+    return prices_offers
+
+def checkout(skus: str) -> int:
     raise NotImplementedError()
+
