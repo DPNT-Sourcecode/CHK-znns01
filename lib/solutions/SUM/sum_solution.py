ca1a40ab964a4afbb8324e6cf9c4cb98cfb12e35
sbx_user1051 <sbx_user1051@169.254.235.49>
2022-05-10 22:56:44 +0000

    Tue May 10 22:56:44 UTC 2022

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,8 +1,14 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x: int, y: int) -> int:
     if type(x) == int and type(y) == int:
-        return x + y
+        if (x >= 0 and x <= 100) and (y >= 0 and y <= 100):
+            return x + y
+        else:
+            raise Exception(
+                f"Unexpected value for x: {x} or y: {y}."
+                " Their value has to be between 0 and 100.")
     else:
         raise Exception(f"Unexpected type for x: {x} or y: {y}.")
+
 
 
