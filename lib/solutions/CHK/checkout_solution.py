325503e7be1139529fc45bcf4faaa5d0f4c7faec
sbx_user1051 <sbx_user1051@169.254.235.49>
2022-05-10 23:16:52 +0000

    Tue May 10 23:16:52 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,16 +3,22 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 
-def prices_offers_list() -> list(dict):
-    prices_offers = [
-        {"item": "A", "price": 50, "offer": "3A for 130"},
-        {"item": "B", "price": 30, "offer": "2B for 45"},
-        {"item": "C", "price": 20},
-        {"item": "D", "price": 15},
-    ]
+def prices_offers_dict() -> dict:
+    prices_offers = {
+        "A": {"price": 50, "offer": "3A for 130"},
+        "B": {"price": 30, "offer": "2B for 45"},
+        "C": {"price": 20},
+        "D": {"price": 15},
+    }
 
     return prices_offers
 
 def checkout(skus: str) -> int:
-    raise NotImplementedError()
+    skus_list = skus.split(",")
+    prices_offers = prices_offers_list()
+
+    for sku in skus_list:
+        pass
+
+
 
