19b84a12a337522ce1d5b83eb409389413a8af51
sbx_user1051 <sbx_user1051@169.254.235.49>
2022-05-10 22:53:44 +0000

    Tue May 10 22:53:44 UTC 2022

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,8 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
-def compute(x, y):
-    raise NotImplementedError()
+def compute(x: int, y: int) -> int:
+    if type(x) == int and type(y) == int:
+        return x + y
+    else:
+        raise Exception(f"Unexpected type for x: {x} or y: {y}.")
+
+
