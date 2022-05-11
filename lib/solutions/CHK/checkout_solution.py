4c8dbd57af71cb0e08620891e8c7706b5306fc80
sbx_user1051 <sbx_user1051@169.254.207.37>
2022-05-11 15:42:25 +0000

    Wed May 11 15:42:25 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -13,26 +13,40 @@ def prices_offers_dict() -> dict:
 
     return prices_offers
 
+
 def checkout(skus: str) -> int:
     skus_list = skus.split(",")
     prices_offers = prices_offers_dict()
     total_value = 0
+    product_checkout_dict = {}
 
     for sku in skus_list:
         if len(sku) == 0 or len(sku) > 1:
             return -1
-        elif sku in prices_offers.index():
+        elif sku not in prices_offers.index():
+            return -1
+        else:
             item_dict = prices_offers.get(sku)
 
-            if not "offer" in item_dict.index():
-                total_value =+ item_dict.get("price", 0) 
+            if sku not in product_checkout_dict.index():
+                product_checkout_dict[sku] = {
+                    "quantity": 1,
+                    "price": item_dict.get("price"),
+                }
+
+                if "offer" in item_dict.index():
+                    item_offer = item_dict.get("offer")
+                    item_offer_list = item_offer.split()
+                    item_offer_quantity = item_offer_list[0]
+                    item_offer_quantity_number = item_offer_quantity[:-1]
+                    item_offer_price = item_offer_list[-1]
+
+                    product_checkout_dict[sku]["offer"] = {
+                        "quantity": item_offer_quantity_number,
+                        "price": item_offer_price
+                    }
             else:
-                item_offer = item_dict.get("offer")
-                item_offer_list = item_offer.split()
-                item_offer_quantity = item_offer_list[0]
-                item_offer_quantity_number = item_offer_quantity[:-1]
-                item_offer_price = item_offer_list[-1]
-        else:
-            return -1
+                product_checkout_dict[sku]["quantity"] += 1 
 
     return total_value
+
