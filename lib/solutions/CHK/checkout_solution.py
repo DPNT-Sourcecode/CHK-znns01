5bb5f549699c3cb3be328067f6786f46d4edfa0c
sbx_user1051 <sbx_user1051@169.254.12.141>
2022-05-13 01:10:43 +0000

    Fri May 13 01:10:43 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -37,11 +37,11 @@ class Receipt():
                             stripped_item_offer = item_offer.strip(" ")
 
                             if stripped_item_offer[-3] == "one" and stripped_item_offer[-1] == "free":   
-                                product_offered = stripped_item_offer[-2]
+                                product_offered_sku = stripped_item_offer[-2]
 
                                 product_checkout_dict[sku].discounts.append(BundleGiftDiscount(
-                                    bundled_product=product_offered,
-                                    price=item_offer_price
+                                    bundled_product_sku=product_offered_sku,
+                                    quantity=item_offer_quantity_number
                                 ))
                             else:
                                 item_offer_price = item_offer_list[-1]
@@ -149,15 +149,14 @@ class BundlePriceDiscount(Discount):
 class BundleGiftDiscount(Discount):
     # X B Products get one C Product free
     quantity: int
-    price: int
-    bundled_product: Product
+    bundled_product_sku: str
 
     def rule(self, product_quantity: int, product_price, products_dict: dict) -> Decimal:
-        bundled_product_price = self.bundled_product.price
+        bundled_product_price = self.products_dict[bundled_product_sku].price
         applied_discount = 0
 
-        if self.bundled_product.sku in products_dict.keys():
-            offer_quantity_result = Decimal(str(product_quantity)) / Decimal(str(quantity))
+        if self.bundled_product_sku in products_dict.keys():
+            offer_quantity_result = Decimal(str(product_quantity)) / Decimal(str(self.quantity))
             truncated_offer_quantity_result = int(offer_quantity_result)
 
             if truncated_offer_quantity_result >= product_quantity: 
@@ -168,7 +167,7 @@ class BundleGiftDiscount(Discount):
                     Decimal(str(-truncated_offer_quantity_result)) * Decimal(str(bundled_product_price))
 
         discounted_price = \
-            Decimal(str(product_quantity)) * Decimal(str(product_price)) - Dedcimal(str(applied_discount))
+            Decimal(str(product_quantity)) * Decimal(str(product_price)) - Decimal(str(applied_discount))
 
         return discounted_price
 
@@ -191,79 +190,9 @@ def prices_offers_dict() -> dict:
     return prices_offers
 
 
-def populate_product_checkout_dict(skus: str) -> dict:
-    stripped_space_skus = skus.replace(" ", "")
-    skus_list = list(stripped_space_skus)
-    prices_offers = prices_offers_dict()
-    product_checkout_dict = {}
-
-    for sku in skus_list:
-        if len(sku) > 1:
-            return -1
-        elif sku not in prices_offers.keys():
-            return -1
-        else:
-            item_dict = prices_offers.get(sku)
-
-            if sku not in product_checkout_dict.keys():
-                product_checkout_dict[sku] = {
-                    "quantity": 1,
-                    "price": item_dict.get("price"),
-                }
-
-                if "offer" in item_dict.keys():
-                    for item_offer in item_dict.get("offer"):
-                        item_offer_list = item_offer.split()
-                        item_offer_quantity = item_offer_list[0]
-                        item_offer_quantity_number = item_offer_quantity[:-1]
-                        item_offer_price = item_offer_list[-1]
-
-                        if "offer" not in product_checkout_dict[sku].keys():
-                            product_checkout_dict[sku]["offer"] = [{
-                                "quantity": item_offer_quantity_number,
-                                "price": item_offer_price
-                            }]
-                        else:
-                            product_checkout_dict[sku]["offer"].append({
-                                "quantity": item_offer_quantity_number,
-                                "price": item_offer_price
-                            })
-            else:
-                product_checkout_dict[sku]["quantity"] += 1 
-
-    return product_checkout_dict
-
-
-def calculate_product_offer_subtotal(product: dict) -> Decimal:
-    product_offer_quantity = product.get("offer").get("quantity")
-    product_offer_price = product.get("offer").get("price")
-
-    offer_quantity_result = \
-        Decimal(str(product.get("quantity"))) / Decimal(str(product_offer_quantity))
-    truncated_offer_quantity_result = int(offer_quantity_result)
-
-    if truncated_offer_quantity_result > 0:
-        subtotal_offer_value = \
-            Decimal(str(truncated_offer_quantity_result)) * Decimal(str(product_offer_price))
-
-        remainder_offer_quantity_result = \
-            Decimal(str(product.get("quantity"))) % Decimal(str(product_offer_quantity))
-        subtotal_remainder_value = \
-            Decimal(str(remainder_offer_quantity_result)) * Decimal(str(product.get("price")))
-        subtotal_value = \
-            Decimal(str(subtotal_offer_value)) + Decimal(str(subtotal_remainder_value))
-    else:
-        subtotal_value = \
-            Decimal(str(product.get("quantity"))) * Decimal(str(product.get("price")))
-    
-    return subtotal_value
-
-
 def checkout(skus: str) -> int:
-    total_value = 0
-
     if len(skus) == 0:
-        return total_value
+        return 0
 
     receipt = Receipt()
 
@@ -275,4 +204,5 @@ def checkout(skus: str) -> int:
     receipt.calculate_total()
 
     return int(receipt.total)
+
 
